6d9f76a1fb4f9271e639a140afaf1e5f461ef983
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-07-17 21:37:20 +0200

    Added APT Bib

---
+++ src/main/webapp/apt.js
@@ -0,0 +1,10 @@
+angular.module("apt")
+    .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
+    .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
+        this.getCoverabilityGraph = function(aptCode) {
+            $http.post(serverEndpoint + "coverabilityGraph", null)
+                .then(function(response) {
+                    return response.coverabilityGraph
+                })
+        }
+    }]);
