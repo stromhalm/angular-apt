5d70d70cadebc3b2b61099e07f31086f60e0b619
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-07-17 22:25:31 +0200

    New azure username

--- src/main/webapp/apt.js
+++ src/main/webapp/apt.js
@@ -1,17 +1,17 @@
 angular.module("apt")
-    .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
-    .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
-        this.getCoverabilityGraph = function(aptCode) {
-            $http.post(serverEndpoint + "coverabilityGraph", null)
-                .then(function(response) {
-                    return response.coverabilityGraph
-                })
-        }
+.value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
+.service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
+    this.getCoverabilityGraph = function(aptCode) {
+        $http.post(serverEndpoint + "coverabilityGraph", null)
+            .then(function(response) {
+                return response.coverabilityGraph
+            })
+    }
 
-        this.getSynthesizedNet = function(aptCode) {
-            $http.post(serverEndpoint + "coverabilityGraph", null)
-                .then(function(response) {
-                    return response.coverabilityGraph
-                })
-        }
-    }]);
+    this.getSynthesizedNet = function(aptCode) {
+        $http.post(serverEndpoint + "coverabilityGraph", null)
+            .then(function(response) {
+                return response.coverabilityGraph
+            })
+    }
+}]);
