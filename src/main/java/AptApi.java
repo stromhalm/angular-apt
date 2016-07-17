dce65e72d3c9e0480dab73ff33469a0466e9a0ed
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-07-17 20:45:17 +0200

    Output APT-Code as JSON

--- src/main/java/AptApi.java
+++ src/main/java/AptApi.java
@@ -1,10 +1,10 @@
 // Import required java libraries
 import java.io.*;
-import java.util.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import org.json.JSONObject;
 import uniol.apt.adt.pn.PetriNet;
 import uniol.apt.analysis.coverability.CoverabilityModule;
 import uniol.apt.generator.cycle.CycleGenerator;
@@ -14,23 +14,22 @@ import uniol.apt.module.impl.*;
 @WebServlet("/api/coverabilityGraph")
 public class AptApi extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String aptCode = request.getParameter("apt");
-		String covOutput = "";
+		JSONObject jsonOut = new JSONObject();
 		CoverabilityModule covMod = new CoverabilityModule();
-		ModuleOutputImpl output = ModuleUtils.getModuleOutput(covMod);
+		ModuleOutputImpl moduleOutput = ModuleUtils.getModuleOutput(covMod);
 
 
 		try {
-
 			/*
 			AptPNParser parser = new AptPNParser();
 			PetriNet pn = parser.parseString(aptCode);
 			ModuleInputImpl input = new ModuleInputImpl();
 			input.setParameter("pn", pn);
 
-			covMod.run(input, output);
+			covMod.run(input, moduleOutput);
 
 			TransitionSystem cov = (TransitionSystem) output.getValue("lts");
 			AptLTSRenderer parser1 = new AptLTSRenderer();
@@ -39,7 +38,7 @@ public class AptApi extends HttpServlet {
 			CycleGenerator cycleGenerator = new CycleGenerator();
 			PetriNet test = cycleGenerator.generateNet(4);
 			AptPNRenderer renderer = new AptPNRenderer();
-			covOutput = renderer.render(test);
+			jsonOut.put("coverabilityGraph", renderer.render(test));
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -47,12 +46,10 @@ public class AptApi extends HttpServlet {
 
 
 		// Set response content type
-		response.setContentType("text/html");
+		response.setContentType("application/javascript");
 
-		// Actual logic goes here.
-		PrintWriter out = response.getWriter();
-		out.println("<h1>Hallo Welt</h1>");
-		out.println(covOutput);
+		PrintWriter output = response.getWriter();
+		output.println(jsonOut.toString());
 	}
 
 	public void destroy() {
