095f1e37a66f4abc563c0667128927491b85c8bd
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-07-20 16:53:33 +0200

    Use POST again

--- src/main/webapp/apt.js
+++ src/main/webapp/apt.js
@@ -2,10 +2,10 @@ angular.module("apt", [])
 .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
 .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
     this.getCoverabilityGraph = function(aptCode) {
-        return $http.get(serverEndpoint + "coverabilityGraph")
+        return $http.post(serverEndpoint + "coverabilityGraph", null)
     }
 
     this.getSynthesizedNet = function(aptCode) {
-        return $http.get(serverEndpoint + "coverabilityGraph")
+        return $http.post(serverEndpoint + "coverabilityGraph", null)
     }
 }]);
