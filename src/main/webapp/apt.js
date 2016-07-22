fe9db0577ed077347a980b2b2c740c982a39ce2d
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-07-22 18:12:34 +0200

    Pass Parameters to API

--- src/main/webapp/apt.js
+++ src/main/webapp/apt.js
@@ -2,10 +2,10 @@ angular.module("apt", [])
 .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
 .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
     this.getCoverabilityGraph = function(aptCode) {
-        return $http.post(serverEndpoint + "coverabilityGraph", null)
+        return $http.post(serverEndpoint + "coverabilityGraph", {apt: aptCode})
     }
 
     this.getSynthesizedNet = function(aptCode) {
-        return $http.post(serverEndpoint + "coverabilityGraph", null)
+        return $http.post(serverEndpoint + "coverabilityGraph", {apt: aptCode})
     }
 }]);
