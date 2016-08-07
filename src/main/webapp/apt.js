3f9110ae2e31ca96a5029f9429ac3003406ec3e5
Adrian <adrian.jagusch@uni-oldenburg.de>
2016-08-07 16:57:33 +0200

    Fixed variable naming

--- src/main/webapp/apt.js
+++ src/main/webapp/apt.js
@@ -2,7 +2,7 @@ angular.module("apt", [])
 .value("serverEndpoint", "http://angular-apt.azurewebsites.net/api/")
 .service("apt", ["$http", "serverEndpoint", function apt($http, serverEndpoint) {
     this.getCoverabilityGraph = function(pn) {
-        return $http.post(serverEndpoint + "coverabilityGraph", {pn: aptCode})
+        return $http.post(serverEndpoint + "coverabilityGraph", {pn: pn})
     }
 
     this.getSynthesizedNet = function(lts, options) {
